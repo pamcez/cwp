034ee36ffe11a7ab421040c7c6456b55ae50a98b
pamcez <pamcez@users.noreply.github.com>
2019-05-02 13:48:06 +1200

    PageTypes folder

---
+++ app/src/LandingPage.php
@@ -0,0 +1,9 @@
+<?php
+
+class LandingPage extends Page
+{
+    private static $description = 'For each main section of the website';
+    private static $db = [];
+    private static $has_one = [];
+
+}
