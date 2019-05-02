4ece0235b7a53b192f14f80d7319fd3625b9fb51
pamcez <pamcez@users.noreply.github.com>
2019-05-02 15:16:45 +1200

    Changes to files

--- app/src/LandingPage.php
+++ app/src/LandingPage.php
@@ -1,9 +1,30 @@
 <?php
+use  Silverstripe\Forms\ToggleCompositeField;
+use  Silverstripe\Forms\TextField;
+use  Silverstripe\Forms\HTMLEditor\HTMLEditorField;
 
 class LandingPage extends Page
 {
     private static $description = 'For each main section of the website';
-    private static $db = [];
+    private static $db = [
+  'SpecialContentHeadline' => "Varchar(64)",
+  "SpecialContent" => "HTMLText"
+  ];
+
+  public function getCMSFields() {
+  $fields = parent::getCMSFields();
+
+  $fields->addFieldsToTab(
+    'Root.SpecialContent',
+    [
+        TextField::create('SpecialContentHeadline'),
+        HTMLEditorField::create('SpecialContent')
+      ]
+  );
+
+  return $fields;
+}
+
     private static $has_one = [];
 
 }
